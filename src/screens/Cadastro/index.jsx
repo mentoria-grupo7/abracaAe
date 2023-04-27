e3b6cf1ea8f641035bffc916ab762208c37b3449
Ludmila Alves Pinto <102269120+ludmilaalvespinto@users.noreply.github.com>
2023-04-26 21:48:01 -0300

    feature cadastro

---
+++ src/screens/Cadastro/index.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import React from React;
+import {styles} from './style';
+
+export const Cadastro= () => {
+    
+    return (
+        <View style={styles.container}>
+            <Text>
+                Tela cadastro
+            </Text>
+        </View>
+     )
+}
