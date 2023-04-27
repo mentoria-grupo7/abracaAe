6b8fd407e05b4bb9014b2ad36a61ad5cc502a107
Augusto <augustosantosfreitas7@gmail.com>
2023-04-26 21:08:22 -0300

    Tela da localizacão ONGs

---
+++ src/screens/OngLocalization/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+
+export const OngLocalization = () => {
+
+    return (
+            <View> 
+                <Text>
+                    Tela de localização
+                </Text>
+            </View>
+        )
+}
