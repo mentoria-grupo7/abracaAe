d5fcf14674fe95e2ce67f36125f0972471b85d3e
Fábio Penha <fabiopenha95@outlook.com>
2023-04-29 00:10:19 -0300

    feat: adding Partners component in App.js

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { Partners } from './src/screens/Partners';
+import { styles } from './src/screens/Partners/style';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Partners />
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
