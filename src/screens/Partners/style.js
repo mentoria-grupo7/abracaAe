df326eb764b33c5e9b850849715c32f3009ecdd3
Fábio Penha <fabiopenha95@outlook.com>
2023-04-29 00:17:01 -0300

    feat: adding style to Partners screen

--- src/screens/Partners/style.js
+++ src/screens/Partners/style.js
@@ -1,7 +1,15 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-    }
-})
+    title: {
+      color: '#69419D',
+      fontSize: 24,
+      fontWeight: 'bold',
+    },
+    subTitle: {
+        color: '#FF6B6B',
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginTop: 48
+      },
+  });
