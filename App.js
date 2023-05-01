26c4413607f69969b0ec7dba4f994c67acd0706e
Bruna Oliveira <79415713+bruna-luc@users.noreply.github.com>
2023-05-01 20:38:49 -0300

    featureMap

--- App.js
+++ App.js
@@ -1,17 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import React from "react"
+import { StyleSheet, Text,
+         View } from "react-native"
+import MapView,{ Marker }from "react-native-maps";
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </SafeAreaView>
-  );
+
+let locationONGs =[
+  {
+    title:'MARCO ZERO',
+    location:{
+      "latitude": -23.55741978287975,
+      "longitude": -46.63602797314525, 
+    }, 
+    description: "só para saber onde estou "
+  }, 
+  {
+    title:'MARCO 3',
+    location:{
+      "latitude": -23.59741978287975,
+      "longitude": -47.70602797314525, 
+    }, 
+    description: "só para saber onde estou "
+  }, 
+]
+
+
+
+
+export default function App(){
+const onRegionChange = (region) =>{
+  console.log(region)};
+
+
+const showLocationsONGs =() =>{
+    return locationONGs.map((item, index)=>{
+      return(
+       
+        <Marker
+        key={index}
+        coordinate={item.location}
+        title={item.title}
+        description={item.description}
+        
+        />
+      )
+    });
+  };  
+  
+  return(
+
+<View style ={styles.container}>
+ <MapView
+  style={styles.map}
+  onRegionChange={onRegionChange}
+  initialRegion ={{
+    "latitude": -23.55741978287975,
+    "latitudeDelta": 0.02395516515318974,
+    "longitude": -46.63602797314525, 
+    "longitudeDelta": 0.012502111494541168
+  }}
+ > 
+{showLocationsONGs()}
+
+ </MapView>
+
+
+</View>
+
+
+
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+
+const styles =StyleSheet.create({
+  container:{
+    flex:1,
+    background:'#fff',
+    justifyContent: 'flex-end'
+    
+    },
+    map:{
+      width:'100%',
+      height:'60%',
+      alignSelf:'center'
+    }
 });
