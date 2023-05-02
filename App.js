4d3722183c6f65ab750b179725059031aaffdcef
Ludmila Alves Pinto <102269120+ludmilaalvespinto@users.noreply.github.com>
2023-05-02 11:39:07 -0300

    ajuste feature perfil cadastro e login

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text,StatusBar, View } from 'react-native';
+
+import {Login} from './src/screens/Login'
+import {Cadastro} from './src/screens/Cadastro'
+import {Perfil} from './src/screens/Perfil'
+
+
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      {/*<Login></Login>*/}
+      {/*<Cadastro></Cadastro>*/}
+      <Perfil> </Perfil>
+
+    </View>
   );
 }
 
@@ -13,5 +23,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    
   },
 });
