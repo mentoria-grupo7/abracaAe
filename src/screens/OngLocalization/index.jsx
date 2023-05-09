c326c2380ddca7161c852ad8ccdc09af83972de8
Raul <raultheotonio@gmail.com>
2023-05-09 20:56:33 -0300

    alteracao da estrutura da pasta

--- src/screens/OngLocalization/index.jsx
+++ src/screens/OngLocalization/index.jsx
@@ -1,12 +1,7 @@
 import React from "react"
-import Map from "../../Components/map"
-
-
+import Map from "../../components/map"
 
 export default function OngLocalization (){
-
-
-
   return(
     <Map/>
   )
